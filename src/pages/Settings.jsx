a434dbade461908696ee08a795175b9b1e6bbe98
Nick Danelia <nikdanelia@gmail.com>
2022-11-25 18:46:17 +0400

    Added settings page

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,28 +1,45 @@
 import React from "react";
-import { AiFillEdit } from "react-icons/ai";
-import { useState } from "react";
+
+import { ImFilePicture } from "react-icons/im";
 function Settings() {
-  const [userEdit, setUserEdit] = useState(false);
-  const [userValue, setUserValue] = useState("Your Name");
-  const handleUserEdit = () => {
-    setUserEdit((prev) => !prev);
-  };
   return (
     <section className="settings-section">
       <div className="info-container">
-        <img
-          className="settings-pfp"
-          src="https://i.pinimg.com/736x/3a/64/69/3a6469083e633a02f132c2dea8538e8f.jpg"
-        />
-        <div className="username-settings">
-          <span
-            className="settings-username-input"
-            contenteditable={userEdit ? "true" : "false"}
-          >
-            {userValue}
-          </span>
-          <AiFillEdit onClick={handleUserEdit} className="edit-name" />
+        <div className="pfp-settings-cont">
+          <img
+            className="settings-pfp"
+            src="https://i.pinimg.com/736x/3a/64/69/3a6469083e633a02f132c2dea8538e8f.jpg"
+          />
+          <input type="file" id="pfp-input" />
+          <label htmlFor="pfp-input">
+            <ImFilePicture className="upload-img-settings" />
+          </label>
         </div>
+        <div className="username-settings-cont settings-cont">
+          <label htmlFor="username-input-settings">Username</label>
+          <input
+            id="username-input-settings"
+            type="text"
+            placeholder="John Smilga"
+          />
+        </div>
+        <div className="email-settings-cont settings-cont">
+          <label htmlFor="email-input-settings">Email</label>
+          <input
+            id="email-input-settings"
+            type="text"
+            placeholder="johnsmilga@gmail.com"
+          />
+        </div>
+        <div className="password-settings-cont settings-cont">
+          <label htmlFor="password-input-settings">Password</label>
+          <input
+            id="password-input-settings"
+            type="password"
+            placeholder="password"
+          />
+        </div>
+        <button className="save-settings">Save</button>
       </div>
     </section>
   );
